9b6c31b5118ba71aa44bbc7e27a547bfa357b363
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-02-28 12:20:42 +0500

    Solve failing tests

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -16,10 +16,14 @@ import java.util.List;
 @NoArgsConstructor
 public class SimpleJDBCRepository {
     public static void main(String[] args) {
-//        User user = new User(null, "John", "Doe", 25);
+        User user = new User(8L, "Jane", "Doe", 25);
         SimpleJDBCRepository repo = new SimpleJDBCRepository();
 //        repo.createUser(user);
         System.out.println(repo.findUserById(1L).getFirstName());
+        System.out.println(repo.findUserByName("Jane").getLastName());
+        System.out.println(repo.findAllUser().size());
+        System.out.println(repo.updateUser(user));
+        repo.deleteUser(2L);
     }
 
     private Connection connection;
@@ -66,7 +70,6 @@ public class SimpleJDBCRepository {
     public User findUserById(Long userId) {
         User user = new User();
         try (
-            Connection connection = CustomDataSource.getInstance().getConnection();
             PreparedStatement ps = connection.prepareStatement(findUserByIdSQL);
         ) {
             ps.setLong(1, userId);
@@ -87,13 +90,9 @@ public class SimpleJDBCRepository {
 
     public User findUserByName(String userName) {
         User user = new User();
-        try {
-            ps = connection.prepareStatement(findUserByNameSQL);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
+        try (
+            PreparedStatement ps = connection.prepareStatement(findUserByNameSQL);
+        ) {
             ps.setString(1, userName);
 
             ResultSet rs = ps.executeQuery();
@@ -113,13 +112,9 @@ public class SimpleJDBCRepository {
         List<User> users = new ArrayList<>();
         User user = new User();
 
-        try {
-            ps = connection.prepareStatement(findAllUserSQL);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
+        try (
+            PreparedStatement ps = connection.prepareStatement(findAllUserSQL);
+        ) {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 user.setId(rs.getLong("id"));
@@ -136,13 +131,9 @@ public class SimpleJDBCRepository {
     }
 
     public User updateUser(User user) {
-        try {
-            ps = connection.prepareStatement(updateUserSQL);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
+        try (
+            PreparedStatement ps = connection.prepareStatement(updateUserSQL);
+        ) {
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
             ps.setInt(3, user.getAge());
@@ -157,13 +148,9 @@ public class SimpleJDBCRepository {
     }
 
     public void deleteUser(Long userId) {
-        try {
-            ps = connection.prepareStatement(deleteUser);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
+        try (
+            PreparedStatement ps = connection.prepareStatement(deleteUser);
+        ) {
             ps.setLong(1, userId);
 
             ps.executeUpdate();
